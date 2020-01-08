09fa54033f4ecea8dca876afd64fd102ec8426a0
Lars Børty Nielsen <lars.borty@gmail.com>
2020-01-08 11:36:17 +0100

    Update data_management.R

--- data_management.R
+++ data_management.R
@@ -10,12 +10,12 @@ library(tidyverse)
 
 total <- NULL
 
-for (i in 1999:2018) {
+for (i in 1999:2017) {
 
 
 # Data --------------------------------------------------------------------
 
-orig_2006 <- read_delim(paste0("sample_data/sample_orig_", i, ".txt"), 
+orig_2006 <- read_delim(paste0("historical_data1_Q4", i, ".txt"), 
                         "|", escape_double = FALSE, col_names = FALSE, 
                         trim_ws = TRUE, col_types = cols(X26 = col_skip()))
 
@@ -45,7 +45,7 @@ orig_2006 %>% rename(id                = X20,
                      seller            = X24,
                      servicer          = X25) -> orig_2006
 
-svcg_2006 <- read_delim(paste0("sample_data/sample_svcg_", i, ".txt"), 
+svcg_2006 <- read_delim(paste0("historical_data1_time_Q4", i, ".txt"), 
                         "|", escape_double = FALSE, col_names = FALSE, 
                         trim_ws = TRUE, col_types = cols_only(X1 = col_guess(),
                                                               X2 = col_guess(),
@@ -71,33 +71,50 @@ svcg_2006 <- svcg_2006 %>%
 
 svcg_2006$temp <- if_else(svcg_2006$delic == 1, svcg_2006$date_info, as.Date(NA))
 
+
 extra <- svcg_2006 %>% 
   group_by(id) %>% 
-  mutate(prev = lag(delic, order_by=id),
-         survived = ifelse(prev != 0 & delic == 0, 1, 0)) %>%
-  summarize(delic_mean   = mean(delic, na.rm=T),
-            delic_binary = mean(delic, na.rm=T) > 0,
-            delic_date   = first(na.omit(temp)),
-            surv_binary  = mean(survived, na.rm=T) > 0,
-            surv         = sum(survived, na.rm=T),
-            max_unpaid   = max(unpaid, na.rm=T),
-            recovered    = last(delic) == 0) %>% 
+  mutate(status   = ifelse(delic == 1,1,0)) %>% 
+  summarize(delic_date = first(na.omit(temp))) %>% 
+  right_join(svcg_2006, by="id") %>% 
+  group_by(id) %>% 
+  mutate(prev     = lag(delic, order_by=id),
+         survived = ifelse(prev != 0 & delic == 0, 1, 0),
+         counter  = row_number(),
+         status   = ifelse(delic_date == temp, 1, NA)) %>% 
+  group_by(status, add = T) %>% 
+  mutate(survival = max(counter)) %>% 
+  ungroup() %>% 
+  group_by(id) %>% 
+  summarize(delic_mean     = mean(delic, na.rm=T),
+            delic_binary   = mean(delic, na.rm=T) > 0,
+            delic_binary0  = mean(delic == 1, na.rm=T) > 0,
+            delic_binary3  = mean(delic == 3, na.rm=T) > 0,
+            delic_binary6  = mean(delic == 6, na.rm=T) > 0,
+            delic_binary12 = mean(delic == 12, na.rm=T) > 0,
+            delic_date     = first(na.omit(temp)),
+            surv_binary    = mean(survived, na.rm=T) > 0,
+            surv           = sum(survived, na.rm=T),
+            survival       = min(survival),
+            max_unpaid     = max(unpaid, na.rm=T),
+            recovered      = last(delic) == 0) %>% 
   mutate(first_complete_stop = ifelse(delic_binary == TRUE & surv_binary == FALSE, TRUE, FALSE),
          recovered           = ifelse(delic_binary == TRUE & recovered == TRUE, TRUE, FALSE))
 
 
 data <- left_join(orig_2006, extra, by = "id")
 data$dataset <- i
+data$datasetq <- "q4"
 
 total <- bind_rows(total, data)
 
 }
 
-total$income<- total$upb / total$term * 100 / ifelse(total$debt_to_income == 999, NA, total$debt_to_income)
+total$income <- total$upb / total$term * 100 / ifelse(total$debt_to_income == 999, NA, total$debt_to_income)
 
 table(total$delic_binary, total$dataset)
 
-save(total, file="total.rdata")
+save(total, file="total_full_q4.rdata")
 
 
 
